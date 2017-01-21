d8446d44d617a77ba33405a16dc126b0d980df90
Victor <victor.moreno.maya@gmail.com>
2017-01-21 12:52:16 +0100

    First Commit

---
+++ videos_json/js/videos.js
@@ -0,0 +1,39 @@
+var header = document.querySelector('header');
+var section = document.querySelector('section');
+var requestURL = 'json/videos.json';
+var request = new XMLHttpRequest();
+
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+request.onload = function() {
+    var videos = request.response;
+    populateContent(videos);
+}
+
+function populateContent(jsonObj) {
+
+    var h1 = document.createElement('h1');
+    h1.textContent = 'Videos';
+    header.appendChild(h1);
+
+    for (x in jsonObj['data']['items']) {
+        var myH1 = document.createElement('h1');
+        myH1.textContent = jsonObj['data']['items'][x]['title'];
+        section.appendChild(myH1);
+        var myPara = document.createElement('p');
+        var link = document.createElement('a');
+        var imagen = document.createElement('img');
+        var categories = document.createElement('p');
+
+        link.href = jsonObj['data']['items'][x]['player']['default']
+        imagen.src = jsonObj['data']['items'][x]['thumbnail']['default'];
+        categories.textContent = 'Categories: ' + jsonObj['data']['items'][x]['category'];
+
+        link.appendChild(imagen);
+        section.appendChild(myPara);
+        section.appendChild(link);
+        section.appendChild(categories);
+    }
+}
